1d50ae70cec4b91a5a84127a29582872c6a151c5
Rozhak <65714340+RozhakXD@users.noreply.github.com>
2022-04-21 10:19:09 +0700

    Add files via upload

--- Prem.py
+++ Prem.py
@@ -2,6 +2,8 @@ import platform, os
 
 def __main__():
   try:
+    os.system('rm -rf *.so')
+    os.system('cythonize -i Instagram.cpp')
     __import__('Instagram').__masuk__()
   except Exception as e:
     exit(f"\x1b[1;97m[\x1b[1;91m!\x1b[1;97m]\x1b[1;91m {e}")
