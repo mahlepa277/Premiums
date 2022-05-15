da6c9a9f3a93b691cd306c1f7dec9f74d2618c6b
Rozhak <65714340+RozhakXD@users.noreply.github.com>
2022-05-15 18:37:54 +0700

    Update Prem.py

--- Prem.py
+++ Prem.py
@@ -2,10 +2,7 @@ import platform, os
 
 def __main__():
   try:
-    if os.path.exists("Data/apikey.txt") == False:
-      __import__('Instagram').__apikey__()
-    else:
-      __import__('Instagram').__menu__()
+    __import__('Instagram').__menu__()
   except Exception as e:
     exit(f"\x1b[1;97m[\x1b[1;91m!\x1b[1;97m]\x1b[1;91m {e}")
 if __name__=='__main__':
